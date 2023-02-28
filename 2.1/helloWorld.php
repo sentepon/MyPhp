d838106cdff38bae961d45ec69ebbca809fff325
unknown <steponpc@mail.ru>
2023-02-28 20:57:45 +0300

    2.1

---
+++ 2.1/helloWorld.php
@@ -0,0 +1,28 @@
+<html>
+
+<head>
+   <title>дз</title>
+   <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+   <header>
+      <div class="container header__container">
+         <img class="header__logo" src="./Politech.png" alt="logo">
+         <h1>Павленко Степан 2.1 Домашняя работа: Hello, World!</h1>
+      </div>
+   </header>
+   <main>
+      <div class="container">
+         <?php echo 'Hello, world!'; ?>
+      </div>
+   </main>
+   <footer>
+      <div class="container">
+         <p>Мои контакты:</p>
+         <a href="mailto:steponpc@mail.ru">steponpc@mail.ru</a>
+      </div>
+   </footer>
+</body>
+
+</html>
